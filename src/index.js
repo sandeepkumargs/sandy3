2a333a24fa08c9ff484d5eaf67514320d4c6920a
goac <goac@goac.thegoacademy.com>
2024-05-11 21:27:28 +0530

    sandeep

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send(
-    " hi iam sandeep, Thought of the day - The only failure is not trying..",
+    " i have done succesfully completed deploying to ecr, Thought of the day - The only failure is not trying..",
   );
 });
 
